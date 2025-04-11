d6e12b9c85e06e1089ef516ed28da3dd03fe3ff9
orangemarmalade88 <orangemarmalade88@gmail.com>
2025-04-11 18:02:53 +0800

    152

--- 152MaximumProductSubarray.cpp
+++ 152MaximumProductSubarray.cpp
@@ -1,9 +1,20 @@
-#include <vector>
-using namespace std;
+#include "common.h"
 
-class Solution {
-    public:
-        int maxProduct(vector<int>& nums) {
-            
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int max_so_far = nums[0];
+        int min_so_far = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            int temp_max = max(nums[i], max(nums[i] * max_so_far, nums[i] * min_so_far));
+            min_so_far = min(nums[i], min(nums[i] * min_so_far, nums[i] * max_so_far));
+            max_so_far = temp_max;
+            result = max(result, max_so_far);
         }
-    };
+        return result;
+    }
+};
--- common.h
+++ common.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
